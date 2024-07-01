0c8b6b26857124803dab0bc9d326f13fbf70b045
BlinSS <47531950+BlinSS@users.noreply.github.com>
2024-07-01 11:19:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,8 +13,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+git clone https://github.com/QiuSimons/luci-app-daed package/dae
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
